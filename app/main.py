93c5814516c94161827641c65aa25c845a393b43
Rahul.v <svmrahul15@gmail.com>
2025-09-16 16:25:35 +0530

    fix

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 
 from .models import Base, engine
 from .models import Csv_input, Offer, Result
-
+from .router import router
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -11,6 +11,8 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "FastAPI running on Render/Railway"}
+
+app.include_router(router, prefix="/api")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
