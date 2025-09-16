8210dc51086f56a613c9ea6f65176d9bf10aa96a
Rahul.v <svmrahul15@gmail.com>
2025-09-16 16:21:48 +0530

    fix

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
